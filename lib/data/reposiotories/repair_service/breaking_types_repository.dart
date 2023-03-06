e504e76736a4b6dbe7ff80edf3eeffcc219fb618
digiborimusik <digiborimusik@gmail.com>
2023-03-06 10:15:48 +0200

    13

--- lib/data/reposiotories/repair_service/breaking_types_repository.dart
+++ lib/data/reposiotories/repair_service/breaking_types_repository.dart
@@ -4,19 +4,24 @@ import 'package:serv_expert_webclient/data/exceptions.dart';
 import 'package:serv_expert_webclient/data/models/repair_service/breaking_type.dart';
 
 abstract class RSBreakingTypesRepository {
-  Future setBreakingType(RSBreakingType rsBreakingType);
   Future<List<RSBreakingType>> breakingTypes({String? categoryId});
+  Future<RSBreakingType> breakingType(String id);
 }
 
 class RSBreakingTypesRepositoryImpl implements RSBreakingTypesRepository {
-  CollectionReference get _ref => FirebaseFirestore.instance.collection('rs_breaking_types');
+  CollectionReference get _ref => FirebaseFirestore.instance.collection('rs_issues');
 
   @override
-  Future setBreakingType(RSBreakingType rsBreakingType) async {
+  Future<List<RSBreakingType>> breakingTypes({String? categoryId}) async {
     try {
-      await _ref.doc(rsBreakingType.id).set(rsBreakingType.toMap());
-    } on FirebaseException catch (e) {
-      if (e.code == 'permission-denied') {
+      Query query = _ref;
+
+      // query = query.where('categoryId', isEqualTo: categoryId, isNull: categoryId == null);
+
+      QuerySnapshot snapshot = await query.get();
+      return snapshot.docs.map((e) => RSBreakingType.fromMap(e.data() as Map<String, dynamic>)).toList();
+    } catch (e) {
+      if (e is FirebaseException && e.code == 'permission-denied') {
         throw PermissionDenied(e.message!);
       } else {
         throw UnknownException(e.toString());
@@ -25,17 +30,16 @@ class RSBreakingTypesRepositoryImpl implements RSBreakingTypesRepository {
   }
 
   @override
-  Future<List<RSBreakingType>> breakingTypes({String? categoryId}) async {
+  Future<RSBreakingType> breakingType(String id) async {
     try {
-      Query query = _ref;
-
-      query = query.where('categoryId', isEqualTo: categoryId, isNull: categoryId == null);
-
-      QuerySnapshot snapshot = await query.get();
-      return snapshot.docs.map((e) => RSBreakingType.fromMap(e.data() as Map<String, dynamic>)).toList();
+      DocumentSnapshot snapshot = await _ref.doc(id).get();
+      if (!snapshot.exists) throw UnexistedResource('RSBreakingType with id $id does not exist');
+      return RSBreakingType.fromMap(snapshot.data() as Map<String, dynamic>);
     } catch (e) {
       if (e is FirebaseException && e.code == 'permission-denied') {
         throw PermissionDenied(e.message!);
+      } else if (e is UnexistedResource) {
+        rethrow;
       } else {
         throw UnknownException(e.toString());
       }
