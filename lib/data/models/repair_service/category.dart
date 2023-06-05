307ffb0ce8ad8d8b9d4b70a914c99cdc48d750ec
digiborimusik <digiborimusik@gmail.com>
2023-06-05 21:39:29 +0300

    lilfix

--- lib/data/models/repair_service/category.dart
+++ lib/data/models/repair_service/category.dart
@@ -1,4 +1,6 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
 
 class Category extends Equatable {
@@ -7,29 +9,29 @@ class Category extends Equatable {
   final String imageUri;
 
   /// If null, then this is a top-level category else it is a subcategory of the parent category
-  final String? parent;
-  final List<String> issues;
+  final String? parentId;
+  final List<String> issuesIds;
   const Category({
     required this.id,
     required this.name,
     required this.imageUri,
-    this.parent,
-    this.issues = const [],
+    this.parentId,
+    this.issuesIds = const [],
   });
 
   Category copyWith({
     String? id,
     String? name,
     String? imageUri,
-    String? parent,
-    List<String>? issues,
+    String? parentId,
+    List<String>? issuesIds,
   }) {
     return Category(
       id: id ?? this.id,
       name: name ?? this.name,
       imageUri: imageUri ?? this.imageUri,
-      parent: parent ?? this.parent,
-      issues: issues ?? this.issues,
+      parentId: parentId ?? this.parentId,
+      issuesIds: issuesIds ?? this.issuesIds,
     );
   }
 
@@ -38,8 +40,8 @@ class Category extends Equatable {
       'id': id,
       'name': name,
       'imageUri': imageUri,
-      'parent': parent,
-      'issues': issues,
+      'parentId': parentId,
+      'issuesIds': issuesIds,
     };
   }
 
@@ -50,8 +52,10 @@ class Category extends Equatable {
       imageUri: map['imageUri'] == null
           ? 'https://firebasestorage.googleapis.com/v0/b/serv-expert.appspot.com/o/rs_categories%2Fmonitor.png?alt=media&token=e0d5d2da-5701-46f3-bbfb-83a7a5aa622f'
           : map['imageUri'] as String,
-      parent: map['parent'] != null ? map['parent'] as String : null,
-      issues: List<String>.from(map['issues']),
+      parentId: map['parentId'] != null ? map['parentId'] as String : null,
+      issuesIds: List<String>.from(
+        (map['issuesIds']),
+      ),
     );
   }
 
@@ -63,5 +67,13 @@ class Category extends Equatable {
   bool get stringify => true;
 
   @override
-  List<Object> get props => [id, name, parent ?? 0, issues];
+  List<Object> get props {
+    return [
+      id,
+      name,
+      imageUri,
+      parentId ?? 0,
+      issuesIds,
+    ];
+  }
 }
